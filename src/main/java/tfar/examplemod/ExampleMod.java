4bf8e68d2afc06eed9b8b858a040db1e250cc5cc
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-03-11 14:45:01 -0400

    Initial commit

---
+++ src/main/java/tfar/examplemod/ExampleMod.java
@@ -0,0 +1,37 @@
+package tfar.examplemod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ExampleMod.MODID)
+public class ExampleMod
+{
+    // Directly reference a log4j logger.
+
+    public static final String MODID = "examplemod";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ExampleMod() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
