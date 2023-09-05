4664464b6545201ca10a2b269343e8007a8c5e15
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-09-05 16:38:50 -0400

    redo 1.12 modding skeleton because old one was broken

---
+++ README.md
@@ -0,0 +1,2 @@
+# forge-mixin-example
+An example for using Mixin in Minecraft Forge
--- src/main/java/com/example/examplemod/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package com.example.examplemod;
-
-import net.minecraftforge.fml.common.Mod;
-
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(modid = ExampleMod.MODID,name = "Example Mod",version = "@VERSION@")
-public class ExampleMod {
-    public static final String MODID = "examplemod";
-}
