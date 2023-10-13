06a37075b9148d6b21e9ab68c074b599a65e1a5c
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-10-13 19:56:32 -0400

    update 1.19.2 version

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -18,9 +18,11 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod(ExampleMod.MODID)
 public class ExampleMod
 {
+
+    public static final String MODID = "examplemod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
